77e5766dc9de16d4bebf92b4c632765aed77f32e
R3M3KPOL <remigiuszkapleczak@gmail.com>
2022-04-30 00:25:27 +0200

    further work on navigation, and slowly create Main

--- js/main.js
+++ js/main.js
@@ -1,45 +1,71 @@
-const menuLines = document.querySelectorAll('.hamburger__line')
-const menuBtn = document.querySelector('.hamburger__btn')
-const menuList = document.querySelector('.header__items')
+const menuLines = document.querySelectorAll('.hamburger__line');
+const menuBtn = document.querySelector('.hamburger__btn');
+const menuList = document.querySelector('.header__items');
+const ulList = document.querySelector('.header__dropdown');
+const blurBg = document.querySelector('.header__blurBg')
+const dropDown = document.querySelector('.header__item--arrow');
+const closeUlList = document.querySelector('.header__dropdown--closed');
+let mediaQuery = window.matchMedia('(min-width: 768px)');
+
 let flag = true;
+
+
 // viewBox.setAttribute("viewBox", "50 0 100 100")
 
-const showMenu = () => {
+const showMenu =()=> {
     menuLines.forEach((element) => {
         element.classList.toggle('opened');
         if(element.classList.contains('opened')){
-            menuList.classList.add('open')
+            menuList.classList.add('open');
         }
         else if(!element.classList.contains('opened')){
-            menuList.classList.remove('open')
-            
-            
-        }
-       
+            menuList.classList.remove('open');
+            closedUlList()
+
+        } 
     })
 }
 
+const dropDownMenu =()=> {
+    ulList.classList.add('--Mobile');
+    if(ulList.classList.contains('--Mobile')){
+        console.log('ul list')
+        blurBg.classList.add('--open')
+    }
+}
 
-// do przeanalizowania hover.
-// const hoverOver = () => {
+const closedUlList =()=>{
+    ulList.classList.remove('--Mobile')
+    blurBg.classList.remove('--open')
+};
 
-//         menuLines.forEach(() => {
-//             menuLines[0].style.strokeDasharray = '40 207';
-//             menuLines[2].style.strokeDasharray = '60 207';
-           
-//         })
+const removeClass =(e)=> {
+    if(e.target ===blurBg){
+        closedUlList()
+    }
+}
+
+const changeListener =(evt)=>{
+    if(!evt.matches){
+    dropDown.addEventListener("click", dropDownMenu)
     
-// }
-//    const hoverOut = () => {
-//     menuLines.forEach(() => {
-    // menuLines[0].style.strokeDasharray = '60 207';
-    // menuLines[2].style.strokeDasharray = '40 207';
-    
-   
-// })
-// }
+    }
+    else{
+    dropDown.removeEventListener("click",dropDownMenu)
+    closedUlList()
+}
+};
+changeListener(mediaQuery)
 
-menuBtn.addEventListener("click", showMenu)
-// menuBtn.addEventListener('mouseout', hoverOut)
-// btnSvg.addEventListener('mouseover', hoverOver)
+
+
+
+
+
+
+menuBtn.addEventListener("click", showMenu);
+blurBg.addEventListener("click", removeClass)
+mediaQuery.addEventListener("change", changeListener);
+closeUlList.addEventListener("click", closedUlList);
+
   
