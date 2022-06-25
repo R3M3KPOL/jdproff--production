0bb0bb7f78577998787e43c137dc4a120f49746a
R3M3KPOL <remigiuszkapleczak@gmail.com>
2022-06-25 20:54:10 +0200

    I think Im done with the main page

--- js/main.js
+++ js/main.js
@@ -4,12 +4,96 @@ const menuLines = document.querySelectorAll('.btn__line');
 const menuBtn = document.querySelector('.btn__hamburger');
 const menuList = document.querySelector('.header__items');
 const ulList = document.querySelector('.header__dropdown');
-const blurBg = document.querySelector('.header__blurBg')
+const blurBg = document.querySelector('.header__blurBg');
 const dropDown = document.querySelector('.header__item--arrow');
 const closeUlList = document.querySelector('.header__dropdown--closed');
+const btnsSlider = document.querySelectorAll('[data-carousel-button]');
+const slides = document.querySelector("[data-slides]");
 let mediaQuery = window.matchMedia('(min-width: 768px)');
+let size = 100;
 
-// viewBox.setAttribute("viewBox", "50 0 100 100")
+//slider reviews
+// btnsSlider.forEach(button => {
+//     button.addEventListener("click", () => {
+//         const offset = button.dataset.carouselButton === 'next' ? 1: -1;
+//         const activeSlide = slides.querySelector('[data-name="active"]');  
+//         let newIndex = [...slides.children].indexOf(activeSlide) +offset
+//         if(newIndex < 0 ){
+//             newIndex = slides.children.length - 1;
+//         }
+//         if(newIndex >= slides.children.length){
+//             newIndex = 0;
+//         }
+//         if(newIndex < slides.children.length){
+//         slides.style.transform = `translateX(${-size * newIndex}%)`
+        
+//         }
+//         slides.children[newIndex].dataset.name = 'active'
+//         delete activeSlide.dataset.name
+//     })
+
+//     button.addEventListener("keydown",(e) =>{
+//         if(e.code === 'ArrowRight' || e.keyCode === 39){
+//             console.log('arrow')
+//         }
+//     })
+    
+// })
+ const transformX =()=>{
+     if(newIndex < slides.children.length){
+    slides.style.transform = `translateX(${-size * newIndex}%)`
+    slides.style.transition = `transform .3s ease-in-out;`
+     }
+
+}
+ const ifProperty =()=> {
+    if(newIndex < 0 ){
+            newIndex = slides.children.length - 1;
+        }
+    if(newIndex >= slides.children.length){
+            newIndex = 0;
+        }
+ }
+
+
+const slider =(button)=>{
+    const newOffset = button.dataset.carouselButton === "next" ? 1 : -1;
+    
+    button.addEventListener("click", () => {
+        const activeSlide = slides.querySelector('[data-name="active"]'); 
+        offset = newOffset;
+        newIndex = [...slides.children].indexOf(activeSlide) + offset
+        ifProperty()
+        transformX()
+        slides.children[newIndex].dataset.name = 'active'
+        delete activeSlide.dataset.name
+    })
+
+    button.addEventListener("keydown",(elements) =>{
+        const activeSlide = slides.querySelector('[data-name="active"]'); 
+        offset = newOffset;
+    
+        newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if(elements.code === 'ArrowRight' || elements.keyCode === 39 ){
+       ifProperty()
+        transformX()
+    }
+
+        if(elements.code === 'ArrowLeft' || elements.keyCode === 37 ){
+       ifProperty()
+        transformX()
+    }
+        slides.children[newIndex].dataset.name = 'active'
+         delete activeSlide.dataset.name
+                
+        
+    })
+}
+
+
+
+btnsSlider.forEach(slider)
+
 
 const showMenu =()=> {
     menuLines.forEach((element) => {
@@ -61,6 +145,9 @@ root.style.setProperty("--all-elements", figureContent.children.length);
 for(let i=0; i<elementsDisplayed; i++) {
   figureContent.appendChild(figureContent.children[i].cloneNode(true));
 }
+
+
+
 
 
 
